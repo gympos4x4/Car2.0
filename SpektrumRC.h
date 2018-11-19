d10d5680038610fe864b84934fb63150a3350041
Juraj Marcin <juraj@jurajmarcin.com>
2018-11-19 17:04:01 +0100

    Updated Spektrum values, added rear steering control

--- SpektrumRC.h
+++ SpektrumRC.h
@@ -10,19 +10,27 @@
 #include "WProgram.h"
 #endif
 
+#include <Servo.h>
+
 #include "comm.h"
 #include "pindef.h"
 
-#define SR_STR_MIN 990
-#define SR_STR_DMIN 1474
-#define SR_STR_DMAX 1481
-#define SR_STR_MAX 1990
+#define SR_STR_MIN 995
+#define SR_STR_DMIN 1480
+#define SR_STR_DMAX 1495
+#define SR_STR_MAX 1980
 #define SR_THR_MIN 1090
 #define SR_THR_DMIN 1498
 #define SR_THR_DMAX 1507
 #define SR_THR_MAX 1890
 
+#define SR_ASR_MIN 130
+#define SR_ASR_CEN 120
+#define SR_ASR_MAX 70
+
 class _SpektrumRC {
+	
+	Servo aServo;
 
 	int8_t lastThrottle;
 	int8_t lastSteer;
