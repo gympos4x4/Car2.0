43476e8a11c7b56da12c27a14bc10c5740015ad3
Juraj Marcin <justJDev@users.noreply.github.com>
2018-11-19 17:04:01 +0100

    Updated Spektrum values, added rear steering control

--- SpektrumRC.cpp
+++ SpektrumRC.cpp
@@ -8,33 +8,43 @@ _SpektrumRC SpektrumRC;
 void _SpektrumRC::init() {
 	pinMode(THRI_PIN, INPUT);
 	pinMode(STRI_PIN, INPUT);
+	aServo.attach(ASTR_PIN);
 }
 
 int8_t _SpektrumRC::getThrottle() {
 	uint64_t val = pulseIn(THRI_PIN, HIGH, 20000);
+	#ifdef DEBUG
+	Serial.print("THR:");
+	Serial.println((long)val);
+	#endif
 	if (val < SR_THR_DMIN) {
-		return map(val, SR_THR_MIN, SR_THR_DMIN, INT8_MIN, 0);
-	} else if (val < SR_THR_DMAX) {
+		return map(val, SR_THR_MIN, SR_THR_DMIN, -100, 0);
+		} else if (val < SR_THR_DMAX) {
 		return 0;
-	} else {
-		return map(val, SR_THR_DMAX, SR_THR_MAX, 0, INT8_MAX);
+		} else {
+		return map(val, SR_THR_DMAX, SR_THR_MAX, 0, 100);
 	}
 }
 
 int8_t _SpektrumRC::getSteer() {
 	uint64_t val = pulseIn(STRI_PIN, HIGH, 20000);
+	#ifdef DEBUG
+	Serial.print("STR:");
+	Serial.println((long)val);
+	#endif
 	if (val < SR_STR_DMIN) {
-		return map(val, SR_STR_MIN, SR_STR_DMIN, INT8_MIN, 0);
-	} else if (val < SR_STR_DMAX) {
+		return map(val, SR_STR_MIN, SR_STR_DMIN, -100, 0);
+		} else if (val < SR_STR_DMAX) {
 		return 0;
-	} else {
-		return map(val, SR_STR_DMAX, SR_STR_MAX, 0, INT8_MAX);
+		} else {
+		return map(val, SR_STR_DMAX, SR_STR_MAX, 0, 100);
 	}
 }
 
 void _SpektrumRC::loop() {
 	lastThrottle = getThrottle();
 	lastSteer = getSteer();
+	aServo.write(map(lastSteer, -100, 100, SR_ASR_MIN, SR_ASR_MAX));
 }
 
 void _SpektrumRC::updateCarData(class CarData& cardata) {
