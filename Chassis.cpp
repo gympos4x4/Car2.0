6d392864b50cfc95bcf875a992a553f903c0e8e6
Juraj Marcin <juraj@jurajmarcin.com>
2019-01-20 22:48:22 +0100

    implementation

--- Chassis.cpp
+++ Chassis.cpp
@@ -10,11 +10,8 @@ void _Chassis::init() {
 	TCCR2A = (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
 	TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20);
 	OCR2B = 23;
-	// SET OUTPUTS //
-	/*PIN_OUT(CHS_DIR_DIR, CHS_DIR_PIN);
-	PIN_OUT(CHS_PWM_DIR, CHS_PWM_PIN);*/
 	// SET ENDSTOP INPUTS, PULL UPS //
-	/*PIN_IN(DDRD, PD2);
+	PIN_IN(DDRD, PD2);
 	PIN_IN(DDRD, PD3);
 	PIN_WRITE_H(PORTD, PD2);
 	PIN_WRITE_H(PORTD, PD3);
@@ -23,7 +20,7 @@ void _Chassis::init() {
 	PIN_WRITE_L(PORTC, PC6);
 	// SETUP ENDSTOP INTERRUPTS //
 	EICRA = (1 << ISC31) | (1 << ISC30) | (1 << ISC21) | (1 << ISC20);
-	EIMSK = (1 << INT3) | (1 << INT2);*/
+	EIMSK = (1 << INT3) | (1 << INT2);
 	// SET REV SENSOR TIMER //
 	TCCR1A = 0;
 	TCCR1B = (1 << ICES5) | (1 << CS10);
@@ -32,35 +29,21 @@ void _Chassis::init() {
 
 void _Chassis::setHeight(int8_t height) {
 	if (height > 0) {
-		//if (maxHigh) return;
+		if (maxHigh) return;
 		direction = 1;
-		Serial.println(qRotsChange);
 		OCR2B = 31;
 		uint64_t start = millis();
-		while(qRotsChange < CH_STEP/* && millis() < start + CH_TIMEOUT/* && !maxHigh*/) {
-			if (millis() > start + CH_TIMEOUT) {
-				Serial.println("UP TIMEOUT");
-				break;
-			}
-		}
+		while(qRotsChange < CH_STEP && millis() < start + CH_TIMEOUT && !maxHigh);
 		OCR2B = 23;
-		Serial.println(qRotsChange);
-		//maxLow = false;
+		maxLow = false;
 	} else if (height < 0) {
-		//if (maxLow) return;
+		if (maxLow) return;
 		direction = -1;
-		Serial.println(qRotsChange);
 		OCR2B = 16;
 		uint64_t start = millis();
-		while(qRotsChange < CH_STEP/* && millis() < start + CH_TIMEOUT/* && !maxLow*/) {
-			if (millis() > start + CH_TIMEOUT) {
-				Serial.println("DOWN TIMEOUT");
-				break;
-			}
-		}
+		while(qRotsChange < CH_STEP && millis() < start + CH_TIMEOUT && !maxLow);
 		OCR2B = 23;
-		Serial.println(qRotsChange);
-		//maxHigh = false;
+		maxHigh = false;
 	}
 	direction = 0;
 	qRotsChange = 0;
